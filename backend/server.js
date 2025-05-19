e84688350064c3ae7a2d49ccaa34f0d1137065e5
Alec Faustino <alecfaustino@outlook.com>
2025-05-19 11:26:12 -0600

    add cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+//middleware
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
