1714ac40629291b1ae1003afeaaf5b90b1f75ce2
Alec Faustino <alecfaustino@outlook.com>
2025-05-22 08:15:40 -0600

    require pool

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const pool = require('./db/db');
 
 //middleware
 app.use(cors());
