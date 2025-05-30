873f092f1a6e6a2f0e425be0fba5754884ec87e0
Alec Faustino <alecfaustino@outlook.com>
2025-05-30 08:01:59 -0600

    move the list api to own file

--- backend/routes/list-api.js
+++ backend/routes/list-api.js
@@ -1,33 +1,21 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const cors = require('cors');
-const pool = require('./db/db');
-
-
-const usersRoutes = require('./routes/users-api');
-
-//middleware
-app.use(cors());
-app.use('/users', usersRoutes);
-
-
+const express = require("express");
+const router = express.Router();
 
 // // get all of the lists belonging to a user
-// app.get('/users/:userId/lists', async (req, res) => {
+// router.get('/users/:userId/lists', async (req, res) => {
 //   const userId = req.params.userId;
-//   const queryString = 
-//     `SELECT 
-//     lists.list_id, 
+//   const queryString =
+//     `SELECT
+//     lists.list_id,
 //     lists.name,
 //     lists.user_id,
 //     lists.household_id,
-//     CASE 
+//     CASE
 //       WHEN lists.user_id IS NOT NULL THEN 'personal'
 //       ELSE 'household'
 //     END AS type
 //     FROM lists
-//     LEFT JOIN household_users 
+//     LEFT JOIN household_users
 //     ON lists.household_id = household_users.household_id
 //     WHERE lists.user_id = $1 OR household_users.user_id = $1`;
 //   const inputs = [userId];
@@ -42,7 +30,4 @@ app.use('/users', usersRoutes);
 // })
 
 //next route is to get list items for the list the user selects GET /lists/:listId/items
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+module.exports = router;
