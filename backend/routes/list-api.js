1f7b4a257254be6e729969f64ade1a8fc15b0233
Alec Faustino <alecfaustino@outlook.com>
2025-05-30 08:04:35 -0600

    mount the list api to route to server file

--- backend/routes/list-api.js
+++ backend/routes/list-api.js
@@ -1,33 +1,5 @@
 const express = require("express");
 const router = express.Router();
 
-// // get all of the lists belonging to a user
-// router.get('/users/:userId/lists', async (req, res) => {
-//   const userId = req.params.userId;
-//   const queryString =
-//     `SELECT
-//     lists.list_id,
-//     lists.name,
-//     lists.user_id,
-//     lists.household_id,
-//     CASE
-//       WHEN lists.user_id IS NOT NULL THEN 'personal'
-//       ELSE 'household'
-//     END AS type
-//     FROM lists
-//     LEFT JOIN household_users
-//     ON lists.household_id = household_users.household_id
-//     WHERE lists.user_id = $1 OR household_users.user_id = $1`;
-//   const inputs = [userId];
-//   try {
-//     const result = await pool.query(query, [userId]);
-//     res.json(result.rows);
-//   } catch (error) {
-//     console.error('Error fetching user lists:', err);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-
-// })
-
 //next route is to get list items for the list the user selects GET /lists/:listId/items
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const pool = require('./db/db');
+const cors = require("cors");
+const pool = require("./db/db");
 
-
-const usersRoutes = require('./routes/users-api');
+const usersRoutes = require("./routes/users-api");
 
 //middleware
 app.use(cors());
-app.use('/users', usersRoutes);
+app.use("/users", usersRoutes);
 
+// router definition
 
+const listApi = require("./routes/list-api");
 
-// // get all of the lists belonging to a user
-// app.get('/users/:userId/lists', async (req, res) => {
-//   const userId = req.params.userId;
-//   const queryString = 
-//     `SELECT 
-//     lists.list_id, 
-//     lists.name,
-//     lists.user_id,
-//     lists.household_id,
-//     CASE 
-//       WHEN lists.user_id IS NOT NULL THEN 'personal'
-//       ELSE 'household'
-//     END AS type
-//     FROM lists
-//     LEFT JOIN household_users 
-//     ON lists.household_id = household_users.household_id
-//     WHERE lists.user_id = $1 OR household_users.user_id = $1`;
-//   const inputs = [userId];
-//   try {
-//     const result = await pool.query(query, [userId]);
-//     res.json(result.rows);
-//   } catch (error) {
-//     console.error('Error fetching user lists:', err);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-
-// })
-
-//next route is to get list items for the list the user selects GET /lists/:listId/items
+// mounting router
+app.use("/api/lists", listApi);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
