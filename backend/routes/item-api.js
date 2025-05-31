5c10ccd0e0489169b7a336e84141d8b44324b732
Alec Faustino <alecfaustino@outlook.com>
2025-05-31 13:29:09 -0600

    add route to add item to list

--- backend/routes/item-api.js
+++ backend/routes/item-api.js
@@ -15,4 +15,47 @@ router.get("/", async (req, res) => {
   }
 });
 
+// add list item to list
+router.post("/:listid", async (req, res) => {
+  // TODO REMOVE HARD CODING OF LIST
+  const listid = req.params.listid;
+  // TODO GET THESE VALUES FROM REQ.BODY IN THE FUTURE
+  const itemname = "oranges";
+  const quantity = 4;
+  const photoUrl = null;
+  const departmentId = 1;
+  const storeId = 1;
+  // TODO ADD VALIDATION THAT USER AUTHORIZED TO LIST
+  // ------ //
+
+  const addItemQuery = `
+    INSERT INTO items (list_id, name, quantity, photo_url, department_id, store_id)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+  `;
+
+  // HARD CODED FOR NOW BUT WILL WORK WITH RES.BODY
+  const addItemValues = [
+    listid,
+    itemname,
+    quantity,
+    photoUrl,
+    departmentId,
+    storeId,
+  ];
+
+  try {
+    const addItemResult = await db.query(addItemQuery, addItemValues);
+    res.status(200).json({
+      message: `Successfully added to list ${listid}: `,
+      item: addItemResult.rows[0],
+    });
+  } catch (error) {
+    console.error("Failed to add list item");
+    res.status(500).json({
+      error: "Server Error Adding Item",
+    });
+  }
+});
+
 module.exports = router;
