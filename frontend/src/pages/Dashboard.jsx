064b315bb4bb25bfea903e825474d87780bafcd6
Alec Faustino <alecfaustino@outlook.com>
2025-06-03 20:19:54 -0600

    simple add list forn

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ const Dashboard = () => {
   const navigate = useNavigate();
   const { userId } = useParams();
   const [lists, setLists] = useState([]);
+  const [listName, setListName] = useState("");
   useEffect(() => {
     const fetchLists = async () => {
       try {
@@ -28,11 +29,35 @@ const Dashboard = () => {
     navigate(`/list/${listId}`);
   };
 
+  const captureListName = (e) => {
+    setListName(e.target.value);
+  };
+
+  const addList = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`http://localhost:8080/api/lists/${userId}`, {
+        name: listName,
+      });
+
+      const result = await axios.get(
+        `http://localhost:8080/api/lists/${userId}`
+      );
+      setLists(result.data.data);
+      setListName("");
+    } catch (error) {
+      console.error("Failed to add new list");
+    }
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
       <h2>My Lists</h2>
-      <button>Add List</button>
+      <h3>Add a List!</h3>
+      <label>ListName</label>
+      <input type="text" onChange={captureListName} value={listName}></input>
+      <button onClick={(e) => addList(e)}>Add</button>
       {lists.map((list) => (
         <DashList
           key={list.list_id}
