d2ae56cbd2de4c0e24a9b4e437f8d4bae249c82e
Alec Faustino <alecfaustino@outlook.com>
2025-06-09 07:49:54 -0600

    get the members of a specific household route

--- backend/routes/household-api.js
+++ backend/routes/household-api.js
@@ -30,4 +30,30 @@ router.get("/", async (req, res) => {
 // get members of a specific household
 // for admin information (delete, edit users etc)
 
+router.get("/:householdId", async (req, res) => {
+  // TODO remove || 1 after postman testing
+  userId = req.session.user_id || 1;
+  // TODO remove || 1 after UI is set up
+  householdId = req.params.householdId || 1;
+  const query = `
+    SELECT users.name, household_users.household_id AS household_id, households.name AS household_name FROM users LEFT JOIN household_users ON users.user_id = household_users.user_id LEFT JOIN households ON household_users.household_id = households.household_id
+    WHERE households.household_id = $1;
+  `;
+  const values = [householdId];
+  try {
+    const result = await db.query(query, values);
+
+    res.status(200).json({
+      message: "Successfully fetched household members",
+      data: result.rows,
+    });
+  } catch (error) {
+    console.error("Error fetching household members: ", error);
+    res.status(500).json({
+      message: "Internal Server Error while fetching household members",
+      error,
+    });
+  }
+});
+
 module.exports = router;
